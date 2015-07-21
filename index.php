3712b59440c296f6a37b4484eef442f6b04301ee
iwelina-popova <iwelina.popova@gmail.com>
2015-07-21 12:43:34 +0300

    Add codeGenerator/code-generator-form.css and codeGenerator.html Edit index.php

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <script type="text/javascript">
-    d=document;
-	d.write('<a href="http://interns.north.icnhost.net/" title="ICN Interns monitoring">' +
-    '<img style="visibility: hidden; display: none"  border="0" alt="ICN Interns monitoring" src="http://interns.north.icnhost.net/test.php?platform=' + navigator.platform +
-    '&codename=' + navigator.appCodeName +
-    '&browservs=' + navigator.appVersion +
-    '&cookies=' + navigator.cookieEnabled +
-    '&language=' + navigator.language +
-    '&width=' + screen.width +
-    '&height=' + screen.height +
-    '"</a>');
+    var website = '',
+        visibilityNoneStyle = 'style="visibility: hidden; display: none"',
+        visibilityStyle = '',
+        d = document;
+
+
+
+    d.write('<a href="http://interns.north.icnhost.net/" title="ICN Interns monitoring">' +
+        '<img id="counter"' + visibilityStyle + ' border="0" alt="ICN Interns monitoring" src="http://interns.north.icnhost.net/test.php?platform=' + navigator.platform +
+        '&codename=' + navigator.appCodeName +
+        '&browservs=' + navigator.appVersion +
+        '&cookies=' + navigator.cookieEnabled +
+        '&language=' + navigator.language +
+        '&width=' + screen.width +
+        '&height=' + screen.height +
+        '&website=' + website +
+        '"</a>');
 </script>
