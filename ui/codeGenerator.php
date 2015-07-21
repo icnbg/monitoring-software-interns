b6c45e5ea6b16d430494275dff59c1791b2279da
iwelina-popova <iwelina.popova@gmail.com>
2015-07-22 00:58:22 +0300

    Edit: - index.html - custom.css - codeGenerator.php

--- ui/codeGenerator.php
+++ ui/codeGenerator.php
@@ -54,94 +54,36 @@
             <div class="left_col scroll-view">
 
                 <div class="navbar nav_title" style="border: 0;">
-                    <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>ICN!</span></a>
+                    <a href="index.html" class="site_title"><i class="fa fa-eye"></i> <span>ICN Monitoring</span></a>
                 </div>
                 <div class="clearfix"></div>
 
+                <!-- menu prile quick info -->
+                <div class="profile">
+                    <div class="profile_info">
+                        <span>Welcome,</span>
+                        <h2>USER</h2>
+                    </div>
+                </div>
+                <!-- /menu prile quick info -->
 
-                <br/>
+                <br />
 
                 <!-- sidebar menu -->
                 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
                     <div class="menu_section">
-                        <h3>General</h3>
                         <ul class="nav side-menu">
-                            <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu" style="display: none">
-                                    <li><a href="index.html">Dashboard</a>
-                                    </li>
-                                    <li><a href="index2.html">Dashboard2</a>
-                                    </li>
-                                    <li><a href="index3.html">Dashboard3</a>
-                                    </li>
-                                </ul>
+                            <li><a href="index.html"><i class="fa fa-home"></i> Home <span class="fa"></span></a>
                             </li>
-                            <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu" style="display: none">
-                                    <li><a href="form.html">General Form</a>
-                                    </li>
-                                    <li><a href="form_advanced.html">Advanced Components</a>
-                                    </li>
-                                    <li><a href="form_validation.html">Form Validation</a>
-                                    </li>
-                                    <li><a href="form_wizards.html">Form Wizard</a>
-                                    </li>
-                                    <li><a href="form_upload.html">Form Upload</a>
-                                    </li>
-                                    <li><a href="form_buttons.html">Form Buttons</a>
-                                    </li>
-                                </ul>
+                            <li><a href="codeGenerator.php"><i class="fa fa-cogs"></i> Code Generation <span class="fa"></span></a>
                             </li>
-                            <li><a><i class="fa fa-desktop"></i> UI Elements <span
-                                    class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu" style="display: none">
-                                    <li><a href="general_elements.html">General Elements</a>
-                                    </li>
-                                    <li><a href="media_gallery.html">Media Gallery</a>
-                                    </li>
-                                    <li><a href="typography.html">Typography</a>
-                                    </li>
-                                    <li><a href="icons.html">Icons</a>
-                                    </li>
-                                    <li><a href="glyphicons.html">Glyphicons</a>
-                                    </li>
-                                    <li><a href="widgets.html">Widgets</a>
-                                    </li>
-                                    <li><a href="invoice.html">Invoice</a>
-                                    </li>
-                                    <li><a href="inbox.html">Inbox</a>
-                                    </li>
-                                    <li><a href="calender.html">Calender</a>
-                                    </li>
-                                </ul>
+                            <li><a><i class="fa fa-pie-chart"></i> Statistics <span class="fa"></span></a>
                             </li>
-                            <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu" style="display: none">
-                                    <li><a href="tables.html">Tables</a>
-                                    </li>
-                                    <li><a href="tables_dynamic.html">Table Dynamic</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span
-                                    class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu" style="display: none">
-                                    <li><a href="chartjs.html">Chart JS</a>
-                                    </li>
-                                    <li><a href="chartjs2.html">Chart JS2</a>
-                                    </li>
-                                    <li><a href="morisjs.html">Moris JS</a>
-                                    </li>
-                                    <li><a href="echarts.html">ECharts </a>
-                                    </li>
-                                    <li><a href="other_charts.html">Other Charts </a>
-                                    </li>
-                                </ul>
+                            <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
                             </li>
                         </ul>
                     </div>
-
 
                 </div>
                 <!-- /sidebar menu -->
@@ -250,7 +192,7 @@ d.write('<a href=\"http://interns.north.icnhost.net/\" title=\"ICN Interns monit
                         <div class="ln_solid"></div>
                         <div class="form-group">
                             <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-                                <button type="submit" id="btn-cancel" class="btn btn-primary">Back</button>
+                                <button onclick="history.go(-1);" type="submit" id="btn-cancel" class="btn btn-primary">Back</button>
                                 <button type="submit" name="submit" class="btn btn-success">Generate</button>
                             </div>
                         </div>
